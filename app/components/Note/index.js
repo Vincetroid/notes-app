b906d0eb6f5c685b20c9e9f8fce1e5f468f1fd42
Vincetroid <vince_trance@hotmail.com>
2021-09-30 00:51:24 -0500

    Add app folder with components and utils folder. Also create Note and NoteDetail folders to store those component definitions.

---
+++ app/components/Note/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Note = () => {
+  console.log(78787878);
+  return (
+    <View>
+      <Text>A Note</Text>
+    </View>
+  );
+};
+
+export default Note;
